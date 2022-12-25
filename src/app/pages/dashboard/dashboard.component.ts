988268b0bee140831c68be782bb27e580953175b
Le Quoc Lap <quoclap.hedspi@gmail.com>
2022-12-25 23:19:56 +0900

    added progress bar

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { SPORT_WEIGHT_MAP } from 'src/app/const';
 import { Activity, Athlete } from 'src/app/datatypes/APIDataType';
 import { SportType } from 'src/app/enum';
 import { AthleteService } from 'src/app/services/athlete.service';
+import { faBicycle, faPersonHiking, faPersonSwimming, faRunning, faSnowboarding } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,7 +17,23 @@ export class DashboardComponent {
   public athletesData: Athlete[] = [];
   public errorMessage = '';
   public totalDistance = 0;
-  public totalDistanceStr = '';
+  public challengeGoal = 1000;
+  public challengeName = "Chao xuan 2023";
+
+  faRide = faBicycle;
+  faHike = faPersonHiking;
+  faSnowboard = faSnowboarding;
+  faRun = faRunning;
+  faSwim = faPersonSwimming;
+
+  public progressMap = new Map();
+  public progress = {
+    ride: 0,
+    snowboard: 0,
+    hike: 0,
+    run: 0,
+    swim: 0,
+  }
 
   constructor(
     private athleteService: AthleteService,
@@ -63,13 +80,29 @@ export class DashboardComponent {
           item.activities.forEach((activity) => {
             // add weight
             activity.distance = activity.distance as number * SPORT_WEIGHT_MAP.get(activity.sportType)!;
+
+            // accumulate to map
+            this.progressMap.set(activity.sportType, (this.progressMap.get(activity.sportType) || 0) + activity.distance)
+
             // count total distance
             this.totalDistance += activity.distance;
             activity.distance = Math.floor(activity.distance / 1000);
           })
         })
 
-        this.totalDistanceStr = new Intl.NumberFormat('en-US').format(this.totalDistance / 1000);
+        this.totalDistance = Math.floor(this.totalDistance / 1000);
+        this.progressMap = this.progressMap;
+
+        this.progress = {
+          ride: this.progressMap.get(SportType.Ride) / 1000 / this.challengeGoal * 100,
+          snowboard: this.progressMap.get(SportType.Snowboard) / 1000 / this.challengeGoal * 100,
+          hike: this.progressMap.get(SportType.Hike) / 1000 / this.challengeGoal * 100,
+          run: this.progressMap.get(SportType.Run) / 1000 / this.challengeGoal * 100,
+          swim: this.progressMap.get(SportType.Swim) / 1000 / this.challengeGoal * 100,
+        }
+
+        console.log(this.progress)
+
 
         // rank athlete by distance
         //TODO
