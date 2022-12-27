0d8372096df0ce600a89e7980045251048c798a8
Le Quoc Lap <quoclap.hedspi@gmail.com>
2022-12-27 13:22:05 +0900

    updated sport's weights

--- src/app/const.ts
+++ src/app/const.ts
@@ -2,8 +2,8 @@ import { SportType } from "./enum";
 
 export const SPORT_WEIGHT_MAP = new Map([
     [SportType.Ride, 1],
-    [SportType.Hike, 5],
-    [SportType.Run, 3],
+    [SportType.Hike, 3],
+    [SportType.Run, 2],
     [SportType.Swim, 10],
     [SportType.Snowboard, 1],
     [SportType.AlpineSki, 1]
