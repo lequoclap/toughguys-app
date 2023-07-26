173d7df652bad447a79d40e5a6301285cb880271
Le Quoc Lap <quoclap.hedspi@gmail.com>
2023-07-26 21:27:48 +0900

    Updated challenge to 3500km

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -23,7 +23,7 @@ export class DashboardComponent {
   public isSyncing = false;
   public isHardSyncing = false;
   public challenge = {
-    goal: 3000,
+    goal: 3500,
     name: 'Hot Summer 2023',
     start: '2023-06-01 00:00:00',
     end: '2023-08-01 00:00:00'
